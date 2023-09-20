ea8caed5d8248fb3ca24e36339b27385398ddcdb
7incho <martin30990@gmail.com>
2023-09-20 20:42:12 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import paymentRoutes from "./routes/payment.routes.js";
+
+const app = express();
+app.use(paymentRoutes);
+
+app.listen(3000);
+console.log("Server in port ", 3000);
